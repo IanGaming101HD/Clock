94b415c1ff5136dee7594e3c12f4ef6b61dc4a40
IanGaming101HD <100050322+IanGaming101HD@users.noreply.github.com>
2023-06-17 11:55:45 +0100

    a

--- src/public/main/script.js
+++ src/public/main/script.js
@@ -15,6 +15,24 @@ seconds.innerText = (0).toLocaleString('en-US', {
     useGrouping: false
 })
 
+let date;
+setInterval(() => {
+    date = new Date()
+    hours.innerText = date.getHours().toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+    })
+    minutes.innerText = date.getMinutes().toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+    })
+    seconds.innerText = date.getSeconds().toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+    })
+}, 1000)
+
+
 const clockContainer = document.getElementById('clock-container')
 clockContainer.addEventListener('contextmenu', (event) => {
     event.preventDefault()
